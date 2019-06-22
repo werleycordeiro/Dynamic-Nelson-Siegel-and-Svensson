abe1ce4e43fa48949e390dca9583b03440239d7f
werleycordeiro <31543812+werleycordeiro@users.noreply.github.com>
2019-06-22 08:49:57 -0300

    Update Yhat.betas.R

--- Yhat.betas.R
+++ Yhat.betas.R
@@ -1,4 +1,4 @@
-Yhat.betas <- function(Y)
+Yhat.betas <- function(Y,Z)
 {
   beta <- solve(t(Z) %*% Z) %*% t(Z) %*% t(Y) # Z=maturities X betas, Z'=betas X maturities, Y=observations X maturities, Y'=maturities X observations
   beta <- t(beta)
